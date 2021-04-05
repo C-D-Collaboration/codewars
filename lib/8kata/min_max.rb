081dff2805c9819cfd94d44dad2ca5e5075a6fa4
Dani Coleman <danicoleman00@gmail.com>
2021-04-05 15:23:54 -0500

    Complete minmax
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

---
+++ lib/8kata/min_max.rb
@@ -0,0 +1,21 @@
+# Create a method minmax_by that accepts a list and a block. The method should return an array containing the min and max list elements, based on their return values from the block
+#
+# Here's a simple example:
+#
+# minmax([3,2,5,4]){|item| item}  #=> [2,5]
+# minmax([2,14,7,9]){|item| item.to_s}  #=> [14,9]
+# If the list is empty, you should return [nil, nil].
+#
+# If you need help, here's a reference:
+#
+# http://www.rubycuts.com/enum-minmax-by
+class EightKata
+  def self.minmax_by list, &block
+    return [nil,nil] if list.empty?
+    list.minmax_by(&block)
+    # result = []
+    # result << list.min
+    # result << list.max
+    # result
+  end
+end
---
+++ spec/8kata_spec.rb
@@ -0,0 +1,16 @@
+# require 'minitest/autorun'
+# require 'minitest/pride'
+require 'rspec'
+require "./lib/8kata/min_max"
+
+RSpec.describe EightKata do
+  it "minmax_by" do
+    list = [13,2,5,4]
+    expect(EightKata.minmax_by(list){|item| item}).to eq([2,13])
+  end
+  
+  it "gets nil with empty array" do
+    list = []
+    expect(EightKata.minmax_by(list){|item| item}).to eq([nil,nil])
+  end
+end

b451753a45400f892e5b67900475ae274eee9907
Dani Coleman <danicoleman00@gmail.com>
2021-04-06 10:41:20 -0500

    Complete skip array challenge
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

---
+++ lib/8kata/skip_array.rb
@@ -0,0 +1,5 @@
+class SkipArray
+  def self.drop_while list, &block
+    list.drop_while(&block)
+  end
+end
--- spec/8kata_spec.rb
+++ spec/8kata_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require "./lib/8kata/min_max"
 require "./lib/8kata/first_elements"
+require "./lib/8kata/skip_array"
 
 RSpec.describe EightKata do
   it "minmax_by" do
@@ -18,5 +19,12 @@ RSpec.describe EightKata do
     n = nil
     expect(FirstElements.first(list)).to eq(0)
     expect(FirstElements.first(list, 3)).to eq([0,1,2])
+  end
+
+  it "can skip over elements as long as block returns true" do
+    list = [0,1,2,3,5,8,13]
+    expect(SkipArray.drop_while(list){|item| item < 3}).to eq([3, 5, 8, 13])
+    expect(SkipArray.drop_while(list){|item| item < 8}).to eq([8, 13])
+    expect(SkipArray.drop_while(list){|item| item < 5}).to eq([5, 8, 13])
   end
 end

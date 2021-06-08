0104ba87246b9ceef112d48b4e27ceecb7dc014f
Dani Coleman <danicoleman00@gmail.com>
2021-06-08 09:01:53 -0600

    Add test to insert a dash between each two odd numbers

--- spec/8kata_spec.rb
+++ spec/8kata_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require "./lib/8kata/min_max"
 require "./lib/8kata/first_elements"
+require "./lib/8kata/insert_dashes"
 
 RSpec.describe EightKata do
   it "minmax_by" do
@@ -18,5 +19,13 @@ RSpec.describe EightKata do
     n = nil
     expect(FirstElements.first(list)).to eq(0)
     expect(FirstElements.first(list, 3)).to eq([0,1,2])
+  end
+
+  it "inserts a dash between each two odd numbers" do
+    expect(InsertDashes.insert_dash(454793)).to eq('4547-9-3')
+    expect(InsertDashes.insert_dash(123456)).to eq('123456')
+    expect(InsertDashes.insert_dash(1003567)).to eq('1003-567')
+    expect(InsertDashes.insert_dash(24680)).to eq('24680')
+    expect(InsertDashes.insert_dash(13579)).to eq('1-3-5-7-9')
   end
 end
